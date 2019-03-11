c199bb8308122a37a7f343f0d4de79dbb9911ff9
Tony Correia <tonyapp5013@gmail.com>
2019-03-11 10:00:04 -0700

    TC view tool initial commit

---
+++ lib/tony_correia_view_tool.rb
@@ -0,0 +1,7 @@
+require "tony_correia_view_tool/version"
+require "tony_correia_view_tool/renderer"
+
+module TonyCorreiaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
